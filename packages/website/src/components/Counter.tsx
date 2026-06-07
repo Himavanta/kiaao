b3249480f26fd7f22d7868a957609807939fce07
seven <monstime@foxmail.com>
2026-06-07 11:06:25 +0800

    -

--- packages/website/src/components/Counter.tsx
+++ packages/website/src/components/Counter.tsx
@@ -1,18 +1,25 @@
 import { define } from "kiaao";
+import style from "./style.module.css";
 
 export default function Counter() {
   const [count, setCount] = define(0);
 
+  console.log(count);
+  console.log(count());
+  console.log(count((v) => v));
+  console.log(count((v) => v)());
+
   return (
-    <div class="counter-card">
+    <div class={style.counter_card}>
       <p>Count: {count((v) => v)}</p>
-      <button class="counter-btn" onClick={() => setCount((p) => p + 1)}>
+      <p>Count: {count}</p>
+      <button class={style.counter_btn} onClick={() => setCount((p) => p + 1)}>
         +1
       </button>
-      <button class="counter-btn" onClick={() => setCount((p) => p - 1)}>
+      <button class={style.counter_btn} onClick={() => setCount((p) => p - 1)}>
         -1
       </button>
-      <button class="counter-btn reset" onClick={() => setCount(0)}>
+      <button class={[style.counter_btn, style.reset]} onClick={() => setCount(0)}>
         reset
       </button>
     </div>
